a9b3b0b744e3cebd6353ef8ef11fd936c9d906c5
Kadir03Avci <154731834+Kadir03Avci@users.noreply.github.com>
2023-12-24 23:17:46 +0300

    Add files via upload

---
+++ js/bransJ.js
@@ -0,0 +1,36 @@
+// for header
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+ 
+// for window scroll 
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }
+    else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+    window.onload = () =>{
+        if(window.scrollY > 0){
+            document.querySelector('.feature').classList.add('active');
+        }
+        else{
+            document.querySelector('.feature').classList.remove('active');
+        }
+    }
+}
+
+
